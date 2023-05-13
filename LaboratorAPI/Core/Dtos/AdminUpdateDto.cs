a807bd7c913f3eca4bf34a46914b3eef706c6e6e
bgdalb <bogdanalban2001@gmail.com>
2023-05-13 22:06:45 +0300

    added update dtos

---
+++ LaboratorAPI/Core/Dtos/AdminUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class AdminUpdateDto
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ LaboratorAPI/Core/Dtos/UserUpdateRoleDto.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class UserUpdateRoleDto
+    {
+        [Required]
+        public RoleType Role { get; set; }
+    }
+}
