461314524cf9e10c2886ada25a09f2ccfc092252
bgdalb <bogdanalban2001@gmail.com>
2023-05-13 20:11:25 +0300

    added dbsets for entities

--- LaboratorAPI/DataLayer/AppDbContext.cs
+++ LaboratorAPI/DataLayer/AppDbContext.cs
@@ -16,11 +16,18 @@ namespace DataLayer
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Class>().Property(e => e.Name).HasMaxLength(10);
+            //modelBuilder.Entity<Class>().Property(e => e.Name).HasMaxLength(10);
         }
 
-        public DbSet<Class> Classes { get; set; }
-        public DbSet<Grade> Grades { get; set; }
-        public DbSet<Student> Students { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<User> Users { get; set; }
+      //  public DbSet<Class> Classes { get; set; }
+      //  public DbSet<Grade> Grades { get; set; }
+       // public DbSet<Student> Students { get; set; }
     }
 }
